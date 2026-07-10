940718fc901f144042c1c90627bc9d1d9f212e42
Tamim S <tamimsaleem10@gmail.com>
2026-07-10 16:28:55 -0400

    Update gravitational constant and planet properties
    Added trail mechanics and fixed the orbit to maintain a circular shape while not colliding with the sun in the system. Created functional velocity and acceleration vectors that are visible on screen.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
 || Orbits
 || Creator: Tamim Saleem
-|| Version 1.2 (July 5, 2026) - C++, Raylib
+|| Version 1.3 (July 10, 2026) - C++, Raylib
 || Overview: Simple program that demonstrates planetary orbits
 */
 
@@ -21,10 +21,11 @@ public:
 
 Body sun;    // global
 Body p;
-const float G = 0.667;
+vector<Vector2> trail;
+const float G = 6.67;
 
 void initialize(Body& sun) {
-    sun.mass = 100;
+    sun.mass = 50;
     sun.x = 400;
     sun.y = 400;
     sun.vx = 0;
@@ -34,11 +35,11 @@ void initialize(Body& sun) {
 }
 
 void initialize2(Body& p) {
-    p.mass = 10;
+    p.mass = 1;
     p.x = 400;
     p.y = 500;
     p.vx = 2;
-    p.vy = 1;
+    p.vy = 0;
     p.ax = 0;
     p.ay = 0;
 }
@@ -67,13 +68,13 @@ int main() {
             // gravity = GMm / r^2
             float force = G * sun.mass * p.mass / (dx * dx + dy * dy);
 
-			// acceleration = force / mass
+            // unit vectors
             float directionX = dx / distance;
             float directionY = dy / distance;
 
-			// acceleration in x and y directions
-            p.ax = force * directionX;
-            p.ay = force * directionY;
+			// acceleration in x and y directions (magnitude times direction unit vector)
+            p.ax = (force / p.mass) * directionX;
+            p.ay = (force / p.mass) * directionY;
         }
         
         // Update planet velocity
@@ -84,11 +85,31 @@ int main() {
         p.x += p.vx;
         p.y += p.vy;
 
+        // Drawing trails
+        // blue planet trail
+
+        trail.push_back({p.x, p.y});
+
+        for (int i = 0; i < trail.size(); i++) {
+            DrawCircleV(trail[i], 2.0f, BLUE);
+        }
+
+        // Drawing vectors
+        // velocity
+        DrawLineEx({p.x, p.y}, {p.x + p.vx * 15, p.y + p.vy * 15}, 3.0f, GREEN);
+
+		// acceleration
+        DrawLineEx({p.x, p.y}, {p.x + p.ax * 1500, p.y + p.ay * 1500}, 3.0f, RED);
+
+        // UI
+        // escape text
+		DrawText("Press ESC to exit", 10, 10, 20, WHITE);
+
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawCircle(sun.x, sun.y, 30.0f, YELLOW);
-        DrawCircle(p.x, p.y, 15.0f, BLUE);
+        DrawCircle(sun.x, sun.y, 25.0f, YELLOW);
+        DrawCircle(p.x, p.y, 12.0f, BLUE);
 
 
         EndDrawing();
